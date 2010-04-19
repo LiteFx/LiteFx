fe5dd4ab5cac9708ac2f396364fab64a9dd61c27
Douglas Aguiar <doaguiar@gmail.com>
2010-04-18 22:28:24 -0300

    Test commit.

--- LiteFx.Bases/IIdentificador.cs
+++ LiteFx.Bases/IIdentificador.cs
@@ -5,8 +5,11 @@ using System.Text;
 
 namespace LiteFx.Bases
 {
+    /// <summary>
+    /// To be used when a object needs to bee unique identified.
+    /// </summary>
     public interface IIdentificador
     {
-        decimal Identificador { get; set; }
+        long Identificador { get; set; }
     }
 }
