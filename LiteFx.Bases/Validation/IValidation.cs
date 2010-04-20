ab50cb0c846b4bb6a59be171a6c93b3d1c1db0a0
Douglas Aguiar <doaguiar@gmail.com>
2010-04-20 16:00:51 -0300

    Inicio implementação validation.
    --HG--
    branch : ImplValidation

---
+++ LiteFx.Bases/Validation/IValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
+
+namespace LiteFx.Bases.Validation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IValidation : IDataErrorInfo
+    {
+        /// <summary>
+        /// Resultados das validações realizadas na Bll.
+        /// </summary>
+        ValidationResults Results { get; }
+
+        void ValidateResults();
+    }
+}
