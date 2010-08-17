597d25c5d0c6520379afa8bc763739918e4937e8
Douglas Aguiar <doaguiar@gmail.com>
2010-08-17 14:27:20 -0300

    Arquivo removido por engano.

---
+++ LiteFx.Web.Mvc/Constants.cs
@@ -0,0 +1,14 @@
+﻿﻿namespace LiteFx.Web.Mvc
+ {
+     /// <summary>
+     /// Constantes utilizadas neste projeto
+     /// </summary>
+     internal static class Constants
+     {
+         /// <summary>
+         /// Formato para a montegem de um script no cliente
+         /// </summary>
+         internal const string ScriptFormat = "<script type=\"text/javascript\">//<![CDATA[\n\r{0}\n\r//]]></script>";
+
+     }
+ }
