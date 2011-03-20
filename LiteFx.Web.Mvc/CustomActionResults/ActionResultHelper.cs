8124788588cf4b56b27caadad240e58b30caece9
Douglas Aguiar <doaguiar@gmail.com>
2011-03-20 13:43:44 -0300

    Corrigindo o action result helper.

---
+++ LiteFx.Web.Mvc/CustomActionResults/ActionResultHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace LiteFx.Web.Mvc.CustomActionResults
+{
+    public static class ActionResultHelper
+    {
+        public static string[] MontaCabecalho(object firstRow)
+        {
+            string[] _headers;
+            Type type = firstRow.GetType();
+            _headers = type.GetProperties().Where(p => IsScaffoldColumn(p)).Select(p => p.Name).ToArray();
+
+            return _headers;
+        }
+
+        public static bool IsScaffoldColumn(PropertyInfo pf)
+        {
+            if (pf.IsDefined(typeof(ScaffoldColumnAttribute), true))
+            {
+                ScaffoldColumnAttribute scaffold = (ScaffoldColumnAttribute)pf.GetCustomAttributes(typeof(ScaffoldColumnAttribute), true)[0];
+                return scaffold.Scaffold;
+            }
+
+            return false;
+        }
+
+    }
+
+}
