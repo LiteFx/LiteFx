efcf523de196a61d065b62b6ba411868a40e7dd4
Douglas Aguiar <doaguiar@gmail.com>
2011-04-15 16:19:38 -0300

    Implementacao do adapter para a NHibernate versao 3

---
+++ LiteFx.Bases.NHibernate3/SessionFactoryManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate;
+using System.Reflection;
+using FluentNHibernate.Cfg;
+using NHibernate.Context;
+
+namespace LiteFx.Bases.Context.NHibernate
+{
+    public static class SessionFactoryManager
+    {
+        private static Configuration configuration;
+        /// <summary>
+        /// Propriedade privada para fazer o cache da configuração do NHibernate.
+        /// </summary>
+        public static Configuration Configuration
+        {
+            get { return configuration ?? (configuration = new Configuration()); }
+        }
+
+        /// <summary>
+        /// Has to be setted on constructor.
+        /// </summary>
+        public static Assembly AssemblyToConfigure { get; set; }
+
+        /// <summary>
+        /// Private sessionFactory.
+        /// </summary>
+        private static ISessionFactory sessionFactory;
+
+        /// <summary>
+        /// Propriedade privada para fazer o cache do sessionFactory do NHibernate.
+        /// </summary>
+        public static ISessionFactory SessionFactory
+        {
+            get
+            {
+                return sessionFactory ?? (sessionFactory = Fluently.Configure(Configuration)
+                                        .Mappings(m =>
+                                        {
+                                            m.FluentMappings.AddFromAssembly(
+                                                AssemblyToConfigure);
+                                            m.HbmMappings.AddFromAssembly(
+                                                AssemblyToConfigure);
+                                        })
+                                        .BuildSessionFactory());
+            }
+        }
+
+        private static bool sessionOpened = false;
+
+        internal static ISession GetCurrentSession()
+        {
+            if (!CurrentSessionContext.HasBind(SessionFactoryManager.SessionFactory))
+            {
+                ISession session = SessionFactory.OpenSession();
+                session.BeginTransaction();
+                CurrentSessionContext.Bind(session);
+                sessionOpened = true;
+            }
+            return SessionFactory.GetCurrentSession();
+        }
+
+        public static void DisposeSession()
+        {
+            if (sessionOpened)
+            {
+                var session = GetCurrentSession();
+                session.Close();
+                session.Dispose();
+                CurrentSessionContext.Unbind(SessionFactory);
+                sessionOpened = false;
+            }
+        }
+
+        public static void CommitTransaction()
+        {
+            if (sessionOpened)
+            {
+                var session = GetCurrentSession();
+                if (session.Transaction.IsActive)
+                    session.Transaction.Commit(); 
+            }
+        }
+
+        public static void RollbackTransaction()
+        {
+            if (sessionOpened)
+            {
+                var session = GetCurrentSession();
+                if (session.Transaction.IsActive)
+                    session.Transaction.Rollback(); 
+            }
+        }
+    }
+}
