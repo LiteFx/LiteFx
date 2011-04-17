60b41f85fce6ed830abcd51ff53643981e0214ac
Douglas Aguiar <doaguiar@gmail.com>
2011-04-17 20:16:08 -0300

    Criacao do nuget package 0.9.1

--- LiteFx.Specs/DomainEventsSpecs/OrdinaryEvent.cs
+++ LiteFx.Specs/DomainEventsSpecs/OrdinaryEvent.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace LiteFx.Specs.DomainEventsSpecs
 {
     public class OrdinaryEvent : DomainEvents.IDomainEvent<OrdinarySubject>
