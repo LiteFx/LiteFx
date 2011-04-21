240e968200f13af5abbf52bdbe67fb1e1eabcd9a
Douglas Aguiar <doaguiar@gmail.com>
2011-04-20 21:21:28 -0300

    Teste usando o validation helper em portugues, assim aumenta a cobertura de testes. Nao sei se isto eh legal.

--- LiteFx.Specs/ValidationSpecs/Category.cs
+++ LiteFx.Specs/ValidationSpecs/Category.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using LiteFx.Validation;
+using LiteFx.Validation.PtBr;
 
 namespace LiteFx.Specs.ValidationSpecs
 {
@@ -12,7 +12,7 @@ namespace LiteFx.Specs.ValidationSpecs
 
         public override void ConfigureValidation()
         {
-            Assert<Category>().That(c => c.Name).IsNotNullOrEmpty();
+            Assert<Category>().Que(c => c.Name).NaoSejaNuloOuVazio();
         }
     }
 
@@ -22,7 +22,7 @@ namespace LiteFx.Specs.ValidationSpecs
 
         public override void ConfigureValidation()
         {
-            Assert<SuperCategory>().That(s => s.Rank).Min(1);
+            Assert<SuperCategory>().Que(s => s.Rank).Minimo(1);
             
             base.ConfigureValidation();
         }
