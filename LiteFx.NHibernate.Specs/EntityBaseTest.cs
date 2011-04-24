da2f6f62b69393d712009afd35dbbe47a0a3e259
Douglas Aguiar <doaguiar@gmail.com>
2011-04-24 19:26:32 -0300

    Iniciando os testes do NHibernate.

---
+++ LiteFx.NHibernate.Specs/EntityBaseTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace LiteFx.NHibernate.Specs
+{
+    [TestClass]
+    public class PublicMethodsTest
+    {
+        [TestMethod]
+        public void All_EntityBase_Public_Methods_Should_Be_Virtual()
+        {
+            Assert.IsFalse(HasNonVirtualPublicMethods(typeof(EntityBaseWithValidation<int>)));
+        }
+
+        [TestMethod]
+        public void All_ValueObjectBase_Public_Methods_Should_Be_Virtual()
+        {
+            Assert.IsFalse(HasNonVirtualPublicMethods(typeof(ValueObjectBase)));
+        }
+
+        public bool HasNonVirtualPublicMethods(Type type) 
+        {
+            var methods = from m in type.GetMethods()
+                          where !m.IsVirtual && !m.IsStatic && m.IsPublic && m.DeclaringType != typeof(object)
+                          select m;
+
+            return methods.Count() > 0;
+        }
+    }
+}
