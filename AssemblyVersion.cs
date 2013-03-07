e6c8d9c7d56a5d17fe91bc25051273ae690c42ad
Douglas Aguiar <doaguiar@gmail.com>
2013-03-07 15:46:21 -0300

    mudança de versao para subir pacotes do nuget
    --HG--
    branch : 1.0.0.3-beta

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
-[assembly: AssemblyInformationalVersion("1.0.0.3-alpha")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyInformationalVersion("1.0.0.4-beta")]
