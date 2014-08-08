7c6ddddb4bbe697ca41bbbfe03e13a2cfe7b81c7
Douglas Aguiar <doaguiar@gmail.com>
2014-08-08 18:21:41 -0300

    fechando pacote
    --HG--
    branch : release-2.1.0.0

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
-[assembly: AssemblyInformationalVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyInformationalVersion("2.1.0.0")]
