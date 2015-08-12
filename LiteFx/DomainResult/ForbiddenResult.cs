3ee6b7f61ac94a37551a85f536a12898cf7e025d
Douglas Aguiar <doaguiar@gmail.com>
2015-08-12 14:48:44 -0300

    forbidden result

---
+++ LiteFx/DomainResult/ForbiddenResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiteFx.DomainResult
+{
+	public class ForbiddenResult: IDomainResult<string>
+	{
+		private string body;
+		public string Body
+		{
+			get { return body; }
+		}
+
+		public ForbiddenResult(string message)
+		{
+			body = message;
+		}
+	}
+}
